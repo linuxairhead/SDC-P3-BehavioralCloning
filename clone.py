c69b34a4d1df9e5afcde594b070bf76725d71277
Chang Ki Hong <c_chanho@quicinc.com>
2018-04-22 22:07:20 -0700

    Fifth Training with NVIDIA Architecture

--- clone.py
+++ clone.py
@@ -2,6 +2,7 @@ import csv
 import cv2
 import numpy as np
 
+architecture = 2 # 1 for LeNet, 2 for NVIDIA
 lines = []
 csv_file = '../simulator-self-driving-car/Data2/driving_log.csv'
 with open(csv_file, 'r') as csvfile:
@@ -71,17 +72,34 @@ model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 # cropping the image to remove unnecessary portion of image.
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 
-### training with LeNet Architecture. ###
-model.add(Convolution2D(6,5,5,activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5,activation="relu"))
-model.add(MaxPooling2D())
-model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
-model.add(Dense(1))
+if architecture is 1:
 
-model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=6)
+    ### training with LeNet Architecture. ###
+    model.add(Convolution2D(6,5,5,activation="relu"))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(6,5,5,activation="relu"))
+    model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dense(84))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=6)
+
+elif architecture is 2:
+
+    ### training with NVIDIA Architecture. ###
+    model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(64,3,3, activation="relu"))
+    model.add(Convolution2D(64,3,3, activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=6)
 
 model.save('model.h5')
